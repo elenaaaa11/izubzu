19f4dfcc939f2efdd08ea2cf4ec29c0212261019
Shangzhe2001 <91132717+Shangzhe2001@users.noreply.github.com>
2022-03-24 06:18:30 +0800

    commit #3

--- app/views.py
+++ app/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render, redirect
 from django.db import connection
 
 def index(request):
+    """Shows the main page"""
     return render(request,'app/index.html')
+
+def rent(request):
+
+    ## Rent the house
+    if request.POST:
+        if request.POST['action'] == 'rent':
+            with connection.cursor() as cursor:
+                cursor.execute("UPDATE house_info SET house_status = 'RENTED' WHERE house_title = %s",[request.POST['id']])
+                # Update the record in rent_history
+            
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM house_info ORDER BY expected_price")
+        houses = cursor.fetchall()
+    result_dict = {'records': houses}
+
+    return render(request,'app/rent.html',result_dict)
+                
