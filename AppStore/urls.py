f0cbf268c4cc8fb327a9845a034f5beb6ea7ee6a
Shangzhe2001 <91132717+Shangzhe2001@users.noreply.github.com>
2022-03-25 16:19:47 +0800

    Commit #5

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', app.views.index, name='index'),
     path('rent', app.views.rent, name='rent'),
     path('register', app.views.register, name='register'),
-    #path('post', app.views.register, name='post'),
+    #path('post', app.views.post, name='post'),
     #path('sign_in', app.views.sign_in, name='sign_in'),
     path('admin/', admin.site.urls),
     path('view/<str:title>', app.views.view, name='view')
