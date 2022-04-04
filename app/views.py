dcde4117f673bdd6b51677b1c378a8155cce77a1
elenaaaa11 <99041871+elenaaaa11@users.noreply.github.com>
2022-04-04 14:08:41 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -184,10 +184,12 @@ def rent_1(request, title):
     return render(request, 'app/rent_1.html', result_dict)
 
 
+
+
 def area(request,area_name):
 
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM house_info WHERE house_status = 'FOR RENT' AND area = %s ORDER BY expected_price ", [area_name])
+        cursor.execute("SELECT * FROM house_info WHERE house_status = 'FOR RENT' AND area = %s ORDER BY expected_price ", [request.POST['area_name']])
         houses = cursor.fetchall()
     
     result_dict = {'records': houses}
