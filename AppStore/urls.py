81f75175ae6b5a5fc2ea6159fadfeb9d54e77d08
elenaaaa11 <99041871+elenaaaa11@users.noreply.github.com>
2022-04-04 13:12:57 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('view/<str:title>', app.views.view, name='view'),
     path('rent_1/<str:title>', app.views.rent_1, name='rent_1'),
-    path('view/rent_1/<str:title>', app.views.rent_1, name='rent_2')
+    path('view/rent_1/<str:title>', app.views.rent_1, name='rent_2'),
+    path('area/<str:area>', app.views.area, name='area'),
 ]
